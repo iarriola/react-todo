6d8788104d8fce38590b949ca0f6098b9d563e2e
Ivan Arriola Romero <iarriolar@gmail.com>
2020-11-29 13:44:58 -0600

    Reordering directories

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import {Route, Switch} from 'react-router-dom';
 import './App.css';
 
-import DashboardPage from './dashboard/DashboardPage';
-import AboutPage from './about/AboutPage';
-import Header from './common/Header';
+import DashboardPage from './presentation/dashboard/DashboardPage';
+import AboutPage from './presentation/about/AboutPage';
+import Header from './presentation/common/Header';
 import NotFoundPage from './NotFoundPage';
 import TaskApi from './containers/TaskApi';
 
